e2b2ffe1c57fce10e85751527fc901548ce59ad8
しき中村 <65457534+Blacksun420@users.noreply.github.com>
2022-12-09 16:46:40 -0400

    remove useless var

--- battle/data/AtkDataModel.java
+++ battle/data/AtkDataModel.java
@@ -21,8 +21,6 @@ public class AtkDataModel extends Data implements MaskAtk, BasedCopable<AtkDataM
 	@JsonField(block = true)
 	public final CustomEntity ce;
 	public String str = "";
-	@JsonField(block = true)
-	public String dispStr;
 	public int atk, pre = 1, ld0, ld1, targ = TCH_N, count = -1, dire = 1, alt = 0, move = 0;
 	public boolean range = true;
 	@JsonField(generic = Trait.class, alias = Identifier.class)
