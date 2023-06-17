314cdeabdc17590a84518ac5b45c193e5abeb243
しき中村 <65457534+Blacksun420@users.noreply.github.com>
2023-06-17 19:08:03 -0400

    Return more unlocked data

--- pack/SaveData.java
+++ pack/SaveData.java
@@ -11,6 +11,7 @@ import common.util.unit.Form;
 import common.util.unit.Unit;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.TreeMap;
 
 @JsonClass
@@ -26,38 +27,45 @@ public class SaveData {
         this.pack = pack;
     }
 
-    public byte validClear(Stage st) {
+    public Object[] validClear(Stage st) {
         Integer clm = cSt.get(st.getCont());
         if (clm == null)
             if (st.getCont().unlockReq.isEmpty())
                 cSt.put(st.getCont(), clm = 0);
             else
-                return 0;
+                return null;
         else if (clm > st.getCont().list.indexOf(st))
-            return 0;
+            return null;
         cSt.replace(st.getCont(), clm + 1);
+        Object[] flags = new Object[2];
 
-        byte ret = 1;
+        byte ret = 0;
         if (st.info instanceof CustomStageInfo && ((CustomStageInfo)st.info).reward != null) {
             Form reward = ((CustomStageInfo)st.info).reward;
             Integer ind = ulkUni.get(reward.unit);
-            ulkUni.put(reward.unit, ind == null ? reward.fid : Math.max(ind, reward.fid));
-            ret++;
+            if (ind == null || ind < reward.fid) {
+                ulkUni.put(reward.unit, reward.fid);
+                ret += ind == null ? 1 : 2;
+            }
         }
+        LinkedList<StageMap> newMaps = new LinkedList<>();
         if (clm + 1 == st.getCont().list.size()) //StageMap fully cleared
             for (StageMap sm : st.getCont().getCont().maps)
-                if (!cSt.containsKey(sm)) {
+                if (sm.unlockReq.size() > 0 && !cSt.containsKey(sm)) {
                     boolean addable = true;
                     for (StageMap smp : sm.unlockReq)
                         if (!cSt.containsKey(smp)) {
                             addable = false;
                             break;
                         }
-                    if (addable)
+                    if (addable) {
                         cSt.put(sm, 0);
+                        newMaps.add(sm);
+                    }
                 }
-
-        return ret;
+        flags[0] = ret;
+        flags[1] = newMaps;
+        return flags;
     }
 
     public boolean locked(AbForm f) {
