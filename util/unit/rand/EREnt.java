6a93a56da6796d0f6a61351db638582f77bfc9df
しき中村 <65457534+Blacksun420@users.noreply.github.com>
2023-08-29 00:40:23 -0400

    pain agony even

--- util/unit/rand/EREnt.java
+++ util/unit/rand/EREnt.java
@@ -1,5 +1,9 @@
 package common.util.unit.rand;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import common.io.json.JsonField;
 import common.util.unit.AbEnemy;
 import common.io.json.JsonClass;
 import common.pack.Identifier;
@@ -11,6 +15,7 @@ import org.jetbrains.annotations.Nullable;
 public class EREnt implements BattleStatic, Copable<EREnt> {
 
     @Nullable
+    @JsonField(backCompat = JsonField.CompatType.FORK)
     public Identifier<AbEnemy> ent;
     public int multi = 100;
     public int mula = 100;
@@ -24,5 +29,17 @@ public class EREnt implements BattleStatic, Copable<EREnt> {
         ans.mula = mula;
         ans.share = share;
         return ans;
+    }
+
+    @JsonField(io = JsonField.IOType.W, tag = "ent", backCompat = JsonField.CompatType.UPST)
+    public JsonElement legacyEnt() {
+        if (ent == null)
+            return new JsonNull();
+        JsonObject jo = new JsonObject();
+        jo.addProperty("cls", ent.cls.getName());
+        jo.addProperty("pack", ent.pack);
+        jo.addProperty("id", ent.id);
+        jo.addProperty("_class", Identifier.class.getName());
+        return jo;
     }
 }
