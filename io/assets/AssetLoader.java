e33373c68e88060b6ed7ceba37c8cf946c0a1d48
shitlord <Catsaber9999@gmail.com>
2025-04-11 00:58:07 -0400

    Increase FORK_VER
    I forgot

--- io/assets/AssetLoader.java
+++ io/assets/AssetLoader.java
@@ -71,7 +71,7 @@ public class AssetLoader {
 	}
 
 	public static final String CORE_VER = "0.7.11.0";
-	public static final byte FORK_VER = 11;
+	public static final byte FORK_VER = 12;
 
 	private static final int LEN = 1024;
 
